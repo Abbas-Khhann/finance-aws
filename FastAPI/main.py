1c08a04efe32e6bfc08bc524b7fc51ac695a5172
Abbas Khan <abbaskhhann0@gmail.com>
2024-12-30 19:15:09 +0500

    ports issue

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -9,9 +9,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost"
+    "http://localhost:3000"
 ]
-
 
 app.add_middleware(
     CORSMiddleware,
