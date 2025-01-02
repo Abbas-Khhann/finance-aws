91879f6b65b5fa1acdf4d1df6a8d7a217787d7d6
Abbas Khan <abbaskhhann0@gmail.com>
2025-01-02 19:45:58 +0500

    api.js file updated

--- React/finance-app/src/api.js
+++ React/finance-app/src/api.js
@@ -1,7 +1,17 @@
-import axios from 'axios'
+// import axios from 'axios'
+
+// const api = axios.create({
+//     baseURL: 'http://localhost:8000',
+// });
+
+// export default api;
+
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.NODE_ENV === 'production' 
+        ? 'http://aws-finance-app-env.eba-ijbmy6jm.us-east-1.elasticbeanstalk.com'  // Production URL
+        : 'http://localhost:8000',  // Local development URL
 });
 
 export default api;
